a87c27c37eebda680eff958896eb8178ab12d5ec
Jonathan Saccomano <jonathan.m.saccomano@hotmail.com>
2026-09-01 22:41:32 -0300

    chore: Addressing supabase elements

---
+++ supabase/seed.sql
@@ -0,0 +1,25 @@
+insert into public.products (
+    name,
+    description,
+    price,
+    stock
+)
+values
+(
+    'Hambúrguer',
+    'Hambúrguer artesanal',
+    15.00,
+    100
+),
+(
+    'Batata',
+    'Porção de batata frita',
+    10.00,
+    100
+),
+(
+    'Refrigerante',
+    'Lata 350ml',
+    6.00,
+    100
+);
